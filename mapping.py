177367cca08dd0fc29eb282630605d0d445f43c8
webaction_temp <dimon24@dimon>
2015-06-22 14:43:58 +0300

    removing test page

--- mapping.py
+++ mapping.py
@@ -35,12 +35,6 @@ def number_in_cell(row, column):
         return 0
 
 site_mapping = {
-    "pc_search": {
-        "url": host_url,
-        "computers": ".//*[@id='page-content-wrap']/div/div[1]/div/h1[1]/a",
-        "notebook": ".//*[@id='page-content-wrap']/div[3]/div[1]/div/div[2]/a",
-        "model_number": ".//*[@id='page-content-wrap']/div[3]/div[1]/div[1]/div/div[2]/div[1]/b"
-    },
     "home": {
         "url": "http://gabrielecirulli.github.io/2048",
         "body": "//body",
